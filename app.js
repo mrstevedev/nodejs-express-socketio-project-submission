37fc2ee123c5611aba6097541332b6d9f41a5741
Steven Polido <steven.polido@vistage.com>
2020-01-06 16:35:53 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+const moment = require('moment');
+
+// Idea Service 
+class IdeaService {
+    constructor() {
+        this.ideas = [];
+    }
+    async find() {
+        return this.ideas;
+    }
+    async create(data) {
+        const idea = {
+            id: this.ideas.length,
+            text: data.text,
+            tech: data.tech,
+            viewer: data.viewer
+        }
+        idea.time = moment().format('h:mm:ss a');
+
+        this.ideas.push(idea);
+
+        return idea;
+    }
+}
+
+const app = express(feathers());
+
+// Parse JSON
+app.use(express.json());
+
+// Config Socket.io realtime APIs
+app.configure(socketio());
+// Enable REST services 
+app.configure(express.rest());
+// Register Services
+app.use('/ideas', new IdeaService()); 
+
+app.on('connection', conn => app.channel('stream').join(conn));
+
+app.publish(data => app.channel('stream'));
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT).on('listening', () => console.log(`Server running on ${PORT}`));
+
+// app.service('ideas').create({
+//     text: 'Build a cool app',
+//     tech: 'Nodejs',
+//     viewer: 'John Doe'
+// });
